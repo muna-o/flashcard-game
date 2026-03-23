f54d2ede09ef94ba098bc66f2909f00f34f9ca5a
Munachiso Onyilofor <254061827+muna-o@users.noreply.github.com>
2026-03-23 14:26:36 -0400

    Added const variables to represent buttons and card; implemented displayCard;

--- script.js
+++ script.js
@@ -6,16 +6,35 @@ const flashcards = [
 
 // You can use flashcards.length to get the length of the array
 
+let numOfFlashcards = flashcards.length;
+
 // These two variables will come in handy
 let currentIndex = 0;
 let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
+    if (showingTerm) {
+        card.innerText = flashcards[currentIndex].term;
+    } else {
+        card.innerText = flashcards[currentIndex].definition;
+    }
 
 }
 
 // The rest of the code you will write is apart of event listeners
+
+const submitButton = document.getElementById("add-card-btn");
+const nextButton = document.getElementById("next-btn");
+const prevButton = document.getElementById("prev-btn");
+const card = document.getElementById("card-content");
+
+
+
+submitButton.onclick = () => {
+    flashcards.push("something else");
+    console.log(flashcards);
+}
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
