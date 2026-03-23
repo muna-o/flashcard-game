e16b3b453d37274adfa1a098fc93a3bd0b6d9b8f
Munachiso Onyilofor <254061827+muna-o@users.noreply.github.com>
2026-03-23 15:29:41 -0400

    Finished implementation of event clickers

--- script.js
+++ script.js
@@ -15,25 +15,73 @@ let showingTerm = true;
 // Start with this function to simply display the card
 function displayCard() {
     if (showingTerm) {
-        card.innerText = flashcards[currentIndex].term;
+        cardText.innerText = flashcards[currentIndex].term;
     } else {
-        card.innerText = flashcards[currentIndex].definition;
+        cardText.innerText = flashcards[currentIndex].definition;
     }
-
 }
 
 // The rest of the code you will write is apart of event listeners
 
+// Represents buttons and cards
 const submitButton = document.getElementById("add-card-btn");
 const nextButton = document.getElementById("next-btn");
 const prevButton = document.getElementById("prev-btn");
-const card = document.getElementById("card-content");
+const card = document.getElementById("flashcard");
+const cardText = document.getElementById("card-content");
 
+const newTerm = document.getElementById("new-term");
+const newDefinition = document.getElementById("new-definition");
 
+// Shows definition when card is clicked
+card.onclick = () => {
+    // Turns to opposite value allowing to switc back between front and back
+    showingTerm = !showingTerm;
+    displayCard();
+}
+
+// Moves to previous cards
+prevButton.onclick = () => {
+    // Makes sure that it shows term first when moving to next card
+    showingTerm = true;
+
+    // Decrements the index and loops back to end if it's on first card
+    if (currentIndex == 0) {
+        console.log("Current Index")
+        currentIndex = flashcards.length - 1;
+    } else {
+        currentIndex--;
+    }
+
+    displayCard();
+}
+
+// Move to next cards
+nextButton.onclick = () => {
+    // Makes sure that it shows term first when moving to next card
+    showingTerm = true;
+
+    // Increments the index and loops back to first if it's on last card
+    if (currentIndex == flashcards.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+
+    displayCard();
+}
 
 submitButton.onclick = () => {
-    flashcards.push("something else");
-    console.log(flashcards);
+    // extracts values from input fields
+    let termFieldValue = newTerm.value;
+    let definitionFieldValue = newDefinition.value;
+
+    // Adds object to flashcard array with the values in the input field
+    flashcards.push({ term: termFieldValue, definition: definitionFieldValue });
+
+    // makes input fields empty again 
+    newTerm.value = "";
+    newDefinition.value = "";
 }
 
 // This line will display the card when the page is refreshed
